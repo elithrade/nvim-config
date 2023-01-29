2b4221351f5c567da2bf609e2c0e72281977d17c
Bin Hu <elithrade@gmail.com>
2023-01-29 13:49:22 +0800

    feat/ add gutter signs

--- lua/elithrade/lsp.lua
+++ lua/elithrade/lsp.lua
@@ -97,3 +97,20 @@ mason_lspconfig.setup_handlers {
 
 -- Turn on lsp status information
 require('fidget').setup()
+
+-- Diagnostic symbols in the sign column (gutter)
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●'
+  },
+  update_in_insert = true,
+  float = {
+    source = "always", -- Or "if_many"
+  },
+})
